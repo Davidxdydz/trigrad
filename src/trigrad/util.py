dbf51d5d2600613b17d1615587a090b2ead2fe30
David Berger <david.berger@tum.de>
2025-11-28 10:40:57 +0100

    Add actual values to output

--- src/trigrad/util.py
+++ src/trigrad/util.py
@@ -10,12 +10,12 @@ def create_check(default_device="cuda", default_dtype=precision):
         if device is None:
             device = default_device
         if tensor.device.type != default_device:
-            raise TypeError(f"{name} must be on {default_device}")
+            raise TypeError(f"{name} must be on {default_device}, but got {tensor.device.type}")
         if dtype is None:
             dtype = default_dtype
         if tensor.dtype != dtype:
-            raise TypeError(f"{name} must be of type {dtype}")
+            raise TypeError(f"{name} must be of type {dtype}, but got {tensor.dtype}")
         if shape is not None and tensor.shape != shape:
-            raise ValueError(f"{name} must be of shape {shape}")
+            raise ValueError(f"{name} must be of shape {shape}, but got {tensor.shape}")
 
     return check_tensor
